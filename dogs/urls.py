bb194d07baeee5b1a0b8578b255066342cd120e6
Bezhan-Kirill <bezhan_kiril@mail.ru>
2024-12-20 14:09:54 +0300

    Переопределен метод get_success_url в DogUpdateView. dog_delete_view >> DogDeleteView, внесены изменения в urls

--- dogs/urls.py
+++ dogs/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from dogs.views import index, categories, category_dogs, dog_detail_view, \
-    dog_delete_view, dog_update_view, DogListView, DogCreateView, DogDetailView
+from dogs.views import index, categories, category_dogs, \
+    DogListView, DogCreateView, DogDetailView, DogUpdateView, DogDeleteView
 
 from dogs.apps import DogsConfig
 
@@ -13,6 +13,6 @@ urlpatterns = [
     path('dogs/', DogListView, name='list_dogs'),
     path('dogs/create/', DogCreateView.as_view(), name='create_dog'),
     path('dogs/detail/<int:pk>/', DogDetailView, name='detail_dog'),
-    path('dogs/update/<int:pk>/', dog_update_view, name='update_dog'),
-    path('dogs/delete/<int:pk>/', dog_delete_view, name='delete_dog'),
+    path('dogs/update/<int:pk>/', DogUpdateView.as_view(), name='update_dog'),
+    path('dogs/delete/<int:pk>/', DogDeleteView.as_view(), name='delete_dog'),
 ]
