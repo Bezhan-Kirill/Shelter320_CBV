0e83a74f84c5fd70d34adb3112c568d0e569e3da
Bezhan-Kirill <bezhan_kiril@mail.ru>
2025-01-08 22:12:50 +0300

    Обновлена форма. Исправлены ошибки

--- dogs/forms.py
+++ dogs/forms.py
@@ -25,9 +25,14 @@ class DogAdminForm(StyleFormMixin, forms.ModelForm):
         model = Dog
         fields = '__all__'
 
-    # @staticmethod
-    # def clean_birth_date():
-    #     DogForm.clean_birth_date()
+    def clean_birth_date(self):
+        if self.cleaned_data['birth_date']:
+            cleaned_data = self.cleaned_data['birth_date']
+            now_year = datetime.datetime.now().year
+            if now_year - cleaned_data.year > 100:
+                raise forms.ValidationError('Собака должна быть моложе 100 лет')
+            return cleaned_data
+        return
 
 
 class ParentForm(forms.ModelForm):
